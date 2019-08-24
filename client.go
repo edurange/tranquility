ff9e0d7a0ebf5e66bd80ca24d2c8262d704295e6
Quinn Vinlove <quinnvinlove@skylab.org>
2019-08-23 21:25:35 -0800

    added rest of files

---
+++ client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"os/exec"
+	"os"
+	"encoding/json"
+	"time"
+	"bytes"
+	"strings"
+)
+
+type RO struct {
+	User string `json:"user"`
+	Time int64 `json:"time"`
+	Command string `json:"command"`
+}
+
+func main(){
+	var record RO
+
+	url := os.Getenv("url")
+	uuid := os.Getenv("UUID")
+
+	cmd := exec.Command("whoami")				//current user
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+	record.User = string(out)
+
+	t := time.Now()								//current time
+	record.Time = t.Unix()
+
+	listy := os.Args[1:]
+	record.Command = strings.Join(listy, " ")
+
+	send, _ := json.Marshal(&record)
+
+	logger := &http.Client{}
+	loggerReq, _ := http.NewRequest("POST", url + "/logger", bytes.NewBuffer(send))
+	loggerReq.Header.Add("Content-Type", "application/json")
+	loggerReq.Header.Add("uuid", uuid)
+
+	//send json
+	_, loggerErr := logger.Do(loggerReq)
+	if loggerErr != nil{
+		panic(loggerErr)
+	}
+}
